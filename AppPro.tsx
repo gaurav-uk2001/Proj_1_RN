8c9e6b4ef6884470ae7300f174111a5fc946e6c2
Gaurav Bisht ⚡️ <gaurav.vaishali2001@gmail.com>
2023-11-27 20:39:22 +0530

    Dynamic Theming in React Native: A Guide to Hooks
    >learn in depth what is useColorscheme is

--- AppPro.tsx
+++ AppPro.tsx
@@ -8,6 +8,8 @@ import {
 } from 'react-native'
 
 function AppPro(): JSX.Element{
+    // The useColorScheme React hook provides and subscribes to color scheme updates from the Appearance module.
+    //The Appearance module exposes information about the user's appearance preferences, such as their preferred color scheme (light or dark).
     const isDarkMode = useColorScheme() ==='dark' //default syntax
     return(
         <View style={styles.container}>
@@ -21,11 +23,14 @@ function AppPro(): JSX.Element{
 //creating more custom stylesheet
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        //By default react native has flex-direction as column instead of row as in web. This is mentioned in react native docs. That's why align-items and justify-content behave as opposite to web.
         //styling work on main axis and secondary axis rathar than left and right (used in html) 
+
+        flex: 1,
+
         //align item work left right
         alignItems: 'center',
-        //By default react native has flex-direction as column instead of row as in web. This is mentioned in react native docs. That's why align-items and justify-content behave as opposite to web.
+        // work on top and bottom
         justifyContent: 'center'
     },
     whiteText:{
